cef1de9c5d839fa5404b2937db5b20eb02708a0c
gyojincompany <luckyred@gmail.com>
2022-07-04 12:41:18 +0900

    리액트 연습 예제

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header(props) {  
+  return (
+    <header>
+    <h1><a href="/">REACT</a></h1>
+    <h1>IS GOOD!!!</h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return(
+    <nav>
+      <ol>
+        <li><a href='/'>html</a></li>
+        <li><a href='/'>css</a></li>
+        <li><a href='/'>javascript</a></li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article() {
+  return(
+    <article>
+      <h2>Welcome</h2>
+      Hello, React      
+    </article>
+  );
+}
+
+function Hello(props) {
+  return(
+    <div>
+      안녕하세요 저는 {props.name}입니다. 취미는 {props.hobby}입니다.
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>      
     </div>
   );
 }
