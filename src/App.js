70afa7b2198848bae77fbdcc2845ebc2343ac162
gyojincompany <luckyred@gmail.com>
2022-07-04 15:24:29 +0900

    리액트 연습 예제

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 function Header(props) {  
@@ -17,7 +18,10 @@ function Nav(props) {
 
   for(let i=0;i<props.topics.length;i++) {
     let dto = props.topics[i];
-    list.push(<li key={dto.id}><a href={'/'+dto.id}>{dto.title}</a></li>)
+    list.push(<li key={dto.id}><a id={dto.id} href={'/'+dto.id} onClick={(event)=>{
+      event.preventDefault();
+      props.onClickMode(Number(event.target.id));
+    }}>{dto.title}</a></li>)
   }
   
   return(
@@ -44,18 +48,43 @@ function Article(props) {
 
 
 function App() {
+
+  const [mode, setMode] = useState("Welcome");
+  const [id, setId] = useState(null);
+  
+
   const topics = [
     {id:1, title:"html", body:"html is Good!"},
     {id:2, title:"css", body:"css is Good!"},
     {id:3, title:"javascript", body:"javascript is Good!"}
   ]
 
+  let content = null;
+
+  if(mode === "Welcome") {
+    content = <Article title="Welcome" body="Hello, React"></Article>
+  } else if(mode === "Read") {
+    let title, body = null;
+    for(let i=0;i<topics.length;i++) {
+      console.log(topics[i].id, id);
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  } else if(mode === "Welcome1") {
+    content = <Article title="React!!!" body="React is Good!!!!"></Article>
+  }
 
   return (
     <div>
-      <Header title="React!!" onClickMode={()=>{alert('React is Good!!')}}></Header>
-      <Nav topics={topics}></Nav>
-      <Article title="Welcome" body="Hello, React"></Article>      
+      <Header title="React!!" onClickMode={()=>{setMode("Welcome1");}}></Header>
+      <Nav topics={topics} onClickMode={(id)=>{
+        setMode("Read");
+        setId(id);
+      }}></Nav>
+      {content}      
     </div>
   );
 }
